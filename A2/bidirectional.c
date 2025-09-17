52256793b168b420e8443b3972b3b0a38562c930
Rafay Hassan <rafayhassan02@gmail.com>
2025-09-17 16:40:24 -0400

    Added filed

---
+++ A2/bidirectional.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(){
+
+	int pipe1[2];
+	int pipe2[2];
+	pid_t pid;
+
+	a
+
+	if(pid == 0){
+
+	} else {
+
+	}
+
+return 0;
+
+}//end of main
