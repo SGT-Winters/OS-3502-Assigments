04139c56ce9fe311125a869174aef0555a357201
Rafay Hassan <rafayhassan02@gmail.com>
2025-09-17 15:26:01 -0400

    Completed Producer File

--- A2/producer.c
+++ A2/producer.c
@@ -8,8 +8,53 @@ int main(int argc, char *argv[]){
 
 	FILE *input =stdin;
 	int buffer_size =4096;
-	char opt;
+	char *filename = null;
+	int opt;
 
-//Testing so see if they got added
+	while((opt = getopt(argc, argv, ":f:b:")) != -1){
+
+		switch (opt){
+			case 'f':
+				filename = optarg;
+				break;
+			case 'b':
+				buffer_size = atoi(optarg);
+				if(buffer_size <= 0){
+				fprintf(stderr, "Invalid buffer size: %s\n", optarg);
+				return 1;
+				}
+				break;
+			case '?':
+				printf("ERROR\n");
+				break;
+			case ':':
+				printf("expected argument\n");
+				break;
+		}//end of switch statement
+
+	}//end of while loop
+
+	if(filename){
+		input = fopen(filename, "rb");
+		if(!input){
+			perror("fopen");
+			return 1;
+		}//end of if(!input)
+	}//end of if(filename)
+
+	int *buffer = malloc(buffer_size);
+	if(!buffer){
+		perror("malloc");
+		return 1;
+	}//end of if (!buffer)
+
+	size_t n;
+	while((n = fread(buffer, 1, buffer_size, input)) > 0){
+		fwrite(buffer, 1, n, stdout);
+	}// end of while look
+
+	free(buffer)
+	if(filename) fclose(input);
+
 return 0;
 }
