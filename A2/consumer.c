702012d98c1ac51b8a00e14024bdb6e49f73c8d7
Rafay Hassan <rafayhassan02@gmail.com>
2025-09-17 12:58:22 -0400

    adding consumer file

---
+++ A2/consumer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+#include <time.h>
+
+int main(int argc, char *argv[]{
+
+	int max_line = -1;
+	int verbose = 0;
+
+
+return 0;
+}
