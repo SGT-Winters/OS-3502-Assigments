731cf6e034064144f2b4853b471218133ea7ce99
Rafay Hassan <rafayhassan02@gmail.com>
2025-09-17 15:59:24 -0400

    Finished consumer

--- A2/consumer.c
+++ A2/consumer.c
@@ -1,4 +1,4 @@
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -10,6 +10,38 @@ int main(int argc, char *argv[]{
 	int max_line = -1;
 	int verbose = 0;
 	int opt;
+
+	while((opt = getopt(argc, argv, "n:v")) != -1) {
+		if(opt == 'n){
+			max_line = atoi(optarg);
+		} else if{opt == 'v'){
+			verbose = 1;
+		}
+	}//end of while loop
+
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t n;
+	int line_count;
+	int char_count;
+
+	while ((n = getline(&line, &line, stdin)) != -1){
+		line count++;
+		char_count += n;
+
+		if(verbose){
+			fputs(line, stdout);
+		}
+
+		if(max_line != -1 && line_count >= max_lines){
+			break;
+		}
+	}//end of while loop
+
+	free(line)
+
+	fprintf(stderr, "Lines: %d\n", line_count);
+	fprintf(stderr, "Characters: %d\n", char_count);
 
 return 0;
 }
