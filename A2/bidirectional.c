2754e950a245ce7bc951172c5fe426ea9dfd2667
Rafay Hassan <rafayhassan02@gmail.com>
2025-09-18 13:26:21 -0400

    Finished Parent and Child Pipes

--- A2/bidirectional.c
+++ A2/bidirectional.c
@@ -22,8 +22,45 @@ int main(){
 	}
 
 	if(pid == 0){
+		close(pipe1[1]);
+		close(pipe2[0]);
+
+		char msgs[200];
+		int n = read(pipe1[0], msgs, sizeof(msgs)-1);
+
+		if(n > 0){
+			msgs[n] = '\0';
+			printf("Child got sent: %s\n", msgs);
+			fflush(stdout);
+
+			char responses[] = "Hello from child!";
+			write(pipe2[1], responses, strlen(responses));
+		}
+
+		close(pipe1[0]);
+		close(pipe2[1]);
+		exit(0);
 
 	} else {
+
+		close(pipe1[0]);
+
+		char responses[] = "Hello from parent!";
+		write(pipe1[1], responses, strlen(responses));
+		close(pipe1[1]);
+
+		char msgs[200];
+		int n = read(pipe2[0], msgs, sizeof(msgs)-1);
+		if(n > 0){
+			msgs[n] = '\0';
+			printf("Parent got sent: %s\n", msgs);
+			fflush(stdout);
+		}
+
+		close(pipe2[1]);
+		close(pipe2[0]);
+
+		wait(NULL);
 
 	}
 
